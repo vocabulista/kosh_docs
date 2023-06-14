131d6e75d48cd54299e23795b5c7f8934e640d11
Francisco Mondaca <f.mondaca@uni-koeln.de>
2023-06-14 15:10:19 +0200

    link up

--- theme.config.jsx
+++ theme.config.jsx
@@ -5,7 +5,7 @@ export default {
     project: {
       link: 'https://github.com/cceh/kosh'
     },
-    docsRepositoryBase: 'https://github.com/cceh/kosh_docs/blob/main',
+    docsRepositoryBase: 'https://github.com/vocabulista/kosh_docs/blob/main',
     footer: {
       text: <span>{new Date().getFullYear()} Kosh - APIs for Lexical Data</span>
     }
